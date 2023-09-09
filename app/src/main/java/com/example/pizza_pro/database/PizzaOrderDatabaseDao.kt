c0f1542ed4d42a84ae4f5f34af1ccd18a68ad00b
Pierceid <128907409+Pierceid@users.noreply.github.com>
2023-09-09 15:28:05 +0200

    updating the version 2

--- app/src/main/java/com/example/pizza_pro/database/PizzaOrderDatabaseDao.kt
+++ app/src/main/java/com/example/pizza_pro/database/PizzaOrderDatabaseDao.kt
@@ -5,17 +5,17 @@ import androidx.room.*
 @Dao
 interface PizzaOrderDatabaseDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insertOrder(pizzaOrder: PizzaOrder)
+    suspend fun insertOrder(pizzaOrder: PizzaOrder)
 
     @Delete
-    fun deleteOrder(pizzaOrder: PizzaOrder)
+    suspend fun deleteOrder(pizzaOrder: PizzaOrder)
 
     @Query("SELECT * from pizza_order_table WHERE id = :key")
-    fun get(key: Long): PizzaOrder?
+    suspend fun get(key: Long): PizzaOrder?
 
     @Query("SELECT * from pizza_order_table ORDER BY id DESC LIMIT 1")
     suspend fun getRecent(): PizzaOrder?
 
     @Query("SELECT * FROM pizza_order_table ORDER BY id ASC")
-    fun getAll(): List<PizzaOrder>
+    suspend fun getAll(): List<PizzaOrder>
 }
