5a259498d329db1af31074999042d213ed16786e
Pierceid <128907409+Pierceid@users.noreply.github.com>
2023-09-25 20:27:36 +0200

    updating the version 19

--- app/src/main/java/com/example/pizza_pro/adapter/OrderAdapter.kt
+++ app/src/main/java/com/example/pizza_pro/adapter/OrderAdapter.kt
@@ -30,7 +30,7 @@ class OrderAdapter : RecyclerView.Adapter<OrderAdapter.OrderViewHolder>() {
 
         holder.header.text = String.format("%d. %s", order.id, order.userInfo.name)
         holder.body.text = String.format(
-            "Email: %s\nTime: %s\nPlace: %s\nItems: %d\nCost: %s",
+            "Email: %s\nTime: %s\nPlace: %s\nItems: %d (%s)",
             order.userInfo.email,
             order.time,
             order.place,
