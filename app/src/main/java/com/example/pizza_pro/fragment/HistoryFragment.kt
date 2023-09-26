52b21a07a4c007e1e20e10836723f94b8cc1077d
Pierceid <128907409+Pierceid@users.noreply.github.com>
2023-09-26 12:49:15 +0200

    updating the version 21

--- app/src/main/java/com/example/pizza_pro/fragment/HistoryFragment.kt
+++ app/src/main/java/com/example/pizza_pro/fragment/HistoryFragment.kt
@@ -65,7 +65,7 @@ class HistoryFragment : Fragment(), OnClickListener {
 
     // updates history fragment
     private fun updateHistory(regex: String = binding.etSearchBar.text.toString()) {
-        if (regex.trim().isNotEmpty()) orderViewModel.filterOrders(regex.trim())
+        orderViewModel.filterOrders(regex.trim())
         orderViewModel.orders.observe(viewLifecycleOwner) { newOrders ->
             adapter.initOrders(newOrders)
         }
